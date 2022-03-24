9a216e4d3f55321e9e71f3e1616952f15f2c3d91
Ariel <arielanus1980@yahoo.com.ar>
2022-03-24 15:01:27 -0300

    TP 3-1

---
+++ variables.js
@@ -0,0 +1,72 @@
+var noValgoNiCinco= 4
+console.log (noValgoNiCinco);
+
+var miAnioDeNacimiento= 1980
+var meGustariaTener80Anios= 80
+var voyATener80ElAnio= (miAnioDeNacimiento+meGustariaTener80Anios);
+console.log ("Cumpliré 80 años el año "+ voyATener80ElAnio);
+
+var costoDeUnaTele= 10000
+var ahorros= 9000
+var dineroQueFalta= (costoDeUnaTele-ahorros);
+console.log ("Para comprar la tele me faltan "+dineroQueFalta+" pesos");
+
+var diasPorAnio= 365
+var cantidadDeAniosEnUnaDecada= 10
+var cantidadDeDiasEnUnaDecada= (diasPorAnio*cantidadDeAniosEnUnaDecada);
+console.log ("En una decada hay "+cantidadDeDiasEnUnaDecada+" dias");
+
+var porciones= 24
+const porcionesPorPizza= 8
+var totalDePizzas= (porciones/porcionesPorPizza);
+console.log("Las pizzas en la mesa son "+totalDePizzas);
+
+var miAnimalFavorito= "hamster" == "perro";
+console.log(miAnimalFavorito);
+
+var postre= "flan"
+var superpostre="flan con dulce de leche"
+console.log(postre!=superpostre);
+
+var miEdad= 41
+const edadLimite= 17
+var soyMayorDeEdad =(miEdad > edadLimite);
+console.log(miEdad);
+console.log(soyMayorDeEdad);
+
+var miEdad= 41
+const nuevaEdadLimite= 25
+var soyMenorDe25 =(miEdad<nuevaEdadLimite);
+console.log(miEdad);
+console.log(soyMenorDe25);
+
+var mesActual= "marzo"
+var estamosEnEnero =mesActual==="enero"
+console.log(mesActual);
+console.log(estamosEnEnero);
+
+var mesActual= "marzo"
+var noEstamosEnEnero =mesActual!=="enero"
+console.log(mesActual);
+console.log(noEstamosEnEnero);
+
+
+var miNotaEnElPArcial= 8
+var notaMinimaParaAprobar= 6
+var estoyAprobado=miNotaEnElPArcial>=notaMinimaParaAprobar
+console.log(estoyAprobado);
+
+var horaActual = 11
+var horaDeAlmuerzo= 12
+var esHoraDeAlmorzar= horaActual<=horaDeAlmuerzo
+console.log(esHoraDeAlmorzar);
+
+
+
+
+
+
+
+
+
+
